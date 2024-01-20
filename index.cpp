3b41855db2b4be067fe2ad4d5dbb1fba5c8145aa
Eemaan11 <154345592+Eemaan11@users.noreply.github.com>
2024-01-20 17:02:09 +0500

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header id="home">
+        <nav>
+        <div class="logo">E-Shop</div>
+        <div class="menu">
+        <a href="#home">Home</a>
+        <a href="#featured">Featured</a>
+        <a href="#man">Man</a>
+        <a href="#women">Women</a>
+        <a href="#">Shop</a>
+        </div>
+        </nav>
+        
+    <div class="container">
+    <div class="left">
+    <img src="https://media.istockphoto.com/id/695474472/photo/overhead-shot-of-white-sneakers-on-pink-background.jpg?s=1024x1024&w=is&k=20&c=U6U0mHjZIiLik_WfxLPHbzYCxAwGD-JnZTctRHVl57Q=">
+    </div>
+    <div class="right">
+    <h1>Best Festival Offers</h1>
+    <p>
+    We are providing best quality materials. We have best quality clothes, shoes
+    for man and womens.
+    </p>
+    <p>
+    50% Discount !!!
+    </p>
+<button>Shop</button><br>
+</div>
+</div>
+</header>
+<section id="featured">
+<h1>Featured</h1>
+<div class="wrapper">
+<div class="box1">
+<div>
+<img src="https://media.istockphoto.com/id/1436061606/photo/flying-colorful-womens-sneaker-isolated-on-white-background-fashionable-stylish-sports-shoe.jpg?s=1024x1024&w=is&k=20&c=anQJwG2c4-ZEqf9BgeIm3ph76JZWSU2-GbOE7b_OzcA=">
+<div class="price"> price:$50</div>
+</div>
+<div>
+<img src="https://tse1.mm.bing.net/th?id=OIP.SMKLgFi0xtn2uCagEna2RwHaHa&pid=Api&P=0&h=220">
+<div class="price"> price:$50</div>
+</div>
+<div>
+<img src="https://i5.walmartimages.com/asr/48c782e4-6f05-4ce5-983c-85dcf5bdd4f5_2.0ab5075f9148e854ffad6a4970a140de.jpeg">
+<div class="price"> price:$50</div>
+</div>
+<div>
+<img src="https://tse3.mm.bing.net/th?id=OIP.xbfCFmfNu_BxJLER36OznQHaHa&pid=Api&P=0&h=220">
+<div class="price"> price:$50</div>
+</div>
+<div>
+<img src="https://tse2.mm.bing.net/th?id=OIP.lk6p0wdggUQItz8QwCHyIQHaHa&pid=Api&P=0&h=220">
+<div class="price"> price:$50</div>
+</div>
+</div>
+</section>
+
+    
+<section id="man">
+  <div class="man-row">
+     <div class="man-box">
+        <img src="https://tse1.mm.bing.net/th?id=OIP.SAnNIpybOeFTKuyHXHW9cAHaE8&pid=Api">
+        <h2>Nike Ferr TR<br>$129.99</h2>
+        <h3>Add to cart</h3>
+     </div>
+     <div class="man-box">
+        <img src="https://tse1.mm.bing.net/th?id=OIP.6ZO6xUKIkHiMuPrH-9u7EAHaHa&pid=Api&P=0&h=220">
+        <h2>Nike Ferr TR<br>$129.99</h2>
+        <h3>Add to cart</h3>
+     </div>
+        <div class="man-box">
+            <img src="https://cdn.shopify.com/s/files/1/0085/4468/2061/products/Men-Hiking-Shoes-Comfortable-First-Layer-Cowhide-Leather-Outdoor-Sneakers-Men-Breathable-Hiking-Sports-Boots-Big_5f23ee78-325a-4278-b9b7-c0ed209d5162.jpg?v=1595326402">
+            <h2>Nike Ferr TR<br>$129.99</h2>
+            <h3>Add to cart</h3>
+        </div>
+            <div class="man-box">
+                <img src="https://tse1.mm.bing.net/th?id=OIP.-H0BEb5EJRe1MVEjyC4IcAHaFR&pid=Api&P=0&h=220">
+                <h2>Nike Ferr TR<br>$129.99</h2>
+                <h3>Add to cart</h3>
+            </div>
+  </div>
+</section>
+</div>
+  <section id="man">
+    <div class="man-row">
+  <div class="man-box">
+    <img src="https://images-na.ssl-images-amazon.com/images/I/71iXbvWrx4L._AC_UY675_.jpg">
+    <h2>Nike Ferr TR<br>$129.99</h2>
+    <h3>Add to cart</h3>
+ </div>
+ <div class="women-box">
+    <img src="https://tse4.mm.bing.net/th?id=OIP.ZfQSXciAbirBW5V84ENIXQHaHa&pid=Api&P=0&h=220">
+    <h2>Nike Ferr TR<br>$129.99</h2>
+    <h3>Add to cart</h3>
+ </div>
+    <div class="women-box">
+        <img src="https://i5.walmartimages.com/asr/da931bf0-3564-43c1-b97e-42982f8cc2c1.4a3a92f49918ea91e31e63278e097528.jpeg">
+        <h2>Nike Ferr TR<br>$129.99</h2>
+        <h3>Add to cart</h3>
+    </div>
+        <div class="women-box">
+            <img src="https://tse3.mm.bing.net/th?id=OIP.25dRNeBcoLifdy-WpGuanwHaHa&pid=Api&P=0&h=220">
+            <h2>Nike Ferr TR<br>$129.99</h2>
+            <h3>Add to cart</h3>
+            </div>
+    </div>
+</section>
+
+
+
+
+<section id="footer">
+    <h2>Developed by Ibrahim</h2>
+</section>
+</html>
